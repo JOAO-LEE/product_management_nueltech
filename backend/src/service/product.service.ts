53b460d09895e48808617eee5523ec69ea658ffe
João Lee <joaumlimaum@gmail.com>
2025-06-16 19:56:37 -0300

    feat: create getProductById service function

--- backend/src/service/product.service.ts
+++ backend/src/service/product.service.ts
@@ -2,10 +2,11 @@ import { PrismaClient } from "../../generated/prisma/client";
 const prisma = new PrismaClient();
 
 const getAllProducts = async () => {
-  const products = await prisma.product.findMany();
-  return {
-    data: products,
-  };
+  return await prisma.product.findMany();
 };
 
-export { getAllProducts };
+const getProductById = async (id: number) => {
+  return await prisma.product.findFirst({ where: { id } });
+};
+
+export { getAllProducts, getProductById };
