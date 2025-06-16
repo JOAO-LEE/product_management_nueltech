32c32c23774ae7428b81188bca32355e71ce1ef0
João Lee <joaumlimaum@gmail.com>
2025-06-16 19:44:00 -0300

    chore: update run script

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
-import express, {Response} from 'express';
+import express, { type Response } from "express";
+import { getAllProducts } from "./service/product.service";
 
 const app = express();
 app.use(express.json());
 
-app.get("/", (_req, resp: Response) => {
-  resp.status(200).json({ text: "test" });
+app.get("/", async (_req, resp: Response) => {
+  const products = await getAllProducts();
+  resp.status(200).json({ ...products });
 });
 
 export default app;
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,5 +2,5 @@ import app from "./app";
 import "dotenv/config";
 
 app.listen(process.env.PORT, () => {
-  console.log("APP RUNNING ON PORT 3000")
+  console.log("APP RUNNING ON PORT 3000");
 });
