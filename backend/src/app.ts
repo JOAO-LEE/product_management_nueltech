550bd213ffe21722d3cd6b983c443a3718006442
João Lee <joaumlimaum@gmail.com>
2025-06-16 20:25:44 -0300

    feat: add route to get product by id

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,24 @@
-import express, { type Response } from "express";
-import { getAllProducts } from "./service/product.service";
+import express, {
+  type Response,
+  type Request,
+  type Application,
+} from "express";
+import { getAllProducts, getProductById } from "./service/product.service";
 
-const app = express();
+const app: Application = express();
 app.use(express.json());
 
 app.get("/", async (_req, resp: Response) => {
   const products = await getAllProducts();
   resp.status(200).json({ ...products });
+});
+
+app.get("/:id", async (req: Request, resp: Response) => {
+  const { id } = req.params;
+  const product = await getProductById(+id);
+  if (!product)
+    return resp.status(404).json({ message: "No product were found" });
+  return resp.status(200).json({ product });
 });
 
 export default app;
