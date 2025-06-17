92e337f1c1dce114208b36ef839fe5946a078f4f
João Lee <joaumlimaum@gmail.com>
2025-06-16 21:30:45 -0300

    feat: add update product by id route

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,6 +7,7 @@ import {
   createProduct,
   getAllProducts,
   getProductById,
+  updateProduct,
 } from "./service/product.service";
 import { Product } from "../generated/prisma";
 
@@ -15,7 +16,7 @@ app.use(express.json());
 
 app.get("/", async (_req, resp: Response) => {
   const products = await getAllProducts();
-  resp.status(200).json({ ...products });
+  resp.status(200).json({ data: products });
 });
 
 app.get("/:id", async (req: Request, resp: Response) => {
@@ -28,10 +29,18 @@ app.get("/:id", async (req: Request, resp: Response) => {
 
 app.post("/", async (req: Request, resp: Response) => {
   const product = req.body;
-  const createdProduct = await createProduct(product);
+  await createProduct(product);
+  return resp.status(201).json({ product });
+});
+
+app.put("/:id", async (req: Request, resp: Response) => {
+  const { id } = req.params;
+  const productToUpdate = req.body;
+  const product = await getProductById(+id);
   if (!product)
     return resp.status(404).json({ message: "No product were found" });
-  return resp.status(200).json({ product });
+  const updatedProduct = await updateProduct(productToUpdate);
+  return resp.status(200).json({ updatedProduct });
 });
 
 export default app;
