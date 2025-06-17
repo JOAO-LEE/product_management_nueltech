8f4618e743ae5d5eac5f2ea59f106a8114f3a3fa
João Lee <joaumlimaum@gmail.com>
2025-06-17 14:38:42 -0300

    feat: install and configure react-router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,16 @@
 import "./App.css";
+import { ProductTable } from "./components/ProductTable";
+import { Route, Routes } from "react-router";
 
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <main id="page-content">
+      <Routes>
+        <Route path="/" element={<ProductTable products={products} />} />
+        <Route path="/:id" element={<h1>teste2</h1>} />
+      </Routes>
+    </main>
+  );
 }
 
 export default App;
