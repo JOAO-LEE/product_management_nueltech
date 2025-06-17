d42f040e9696c72fa91b608e18ae8604022b56ab
João Lee <joaumlimaum@gmail.com>
2025-06-17 17:14:37 -0300

    feat: get all products service function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,20 @@
 import "./App.css";
 import { ProductTable } from "./components/ProductTable";
 import { Route, Routes } from "react-router";
+import { InputField, InputRoot } from "./ui/Input/Input";
+import { useEffect, useState } from "react";
+import { fetchAllProducts } from "./service/productService";
+import type { Product } from "./interface/product";
 
 function App() {
+  const [products, setProducts] = useState<Array<Product>>([]);
+
+  useEffect(() => {
+    fetchAllProducts()
+      .then(setProducts)
+      .catch((error) => console.error("Error fetching products:", error));
+  }, []);
+
   return (
     <main id="page-content">
       <Routes>
---
+++ frontend/src/service/productService.ts
@@ -0,0 +1,12 @@
+import type { Product } from "../interface/product";
+const API_BASE_URL = import.meta.env.API_BASE_URL;
+
+export async function fetchAllProducts(): Promise<Product[]> {
+  const response = await fetch(`${API_BASE_URL}`);
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const result = await response.json();
+  return result;
+}
