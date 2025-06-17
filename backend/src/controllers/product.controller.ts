d922e16e45a93a6e6968069004486c296affd0d2
João Lee <joaumlimaum@gmail.com>
2025-06-17 00:42:52 -0300

    refactor: get all products

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,1 +1,11 @@
+import { getProductById } from "../service/product.service";
+import { type Request, type Response } from "express";
 
+const getAll = async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const product = await getProductById(+id);
+  if (!product) return res.status(404).json({ message: "Product not found" });
+  return res.status(200).json({ product });
+};
+
+export { getAll };
