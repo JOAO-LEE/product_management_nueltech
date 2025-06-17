6e77f735f5ee507cb0fc240ccd54dbe5ad158344
João Lee <joaumlimaum@gmail.com>
2025-06-17 17:47:52 -0300

    feat: delete unused imports and fetch functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,24 +1,12 @@
 import "./App.css";
 import { ProductTable } from "./components/ProductTable";
 import { Route, Routes } from "react-router";
-import { InputField, InputRoot } from "./ui/Input/Input";
-import { useEffect, useState } from "react";
-import { fetchAllProducts } from "./service/productService";
-import type { Product } from "./interface/product";
 
 function App() {
-  const [products, setProducts] = useState<Array<Product>>([]);
-
-  useEffect(() => {
-    fetchAllProducts()
-      .then(setProducts)
-      .catch((error) => console.error("Error fetching products:", error));
-  }, []);
-
   return (
     <main id="page-content">
       <Routes>
-        <Route path="/" element={<ProductTable products={products} />} />
+        <Route path="/" element={<ProductTable />} />
         <Route path="/:id" element={<h1>teste2</h1>} />
       </Routes>
     </main>
