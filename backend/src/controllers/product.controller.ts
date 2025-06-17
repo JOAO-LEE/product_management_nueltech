65c6937335e45f0a3becb6b30476523036943ec7
João Lee <joaumlimaum@gmail.com>
2025-06-17 00:51:18 -0300

    fix: get all products controller

--- backend/src/controllers/product.controller.ts
+++ backend/src/controllers/product.controller.ts
@@ -1,18 +1,20 @@
-import { getProductById } from "../service/product.service";
+import {
+  createProduct,
+  getAllProducts,
+  getProductById,
+} from "../service/product.service";
 import { type Request, type Response } from "express";
 
-const getAll = async (req: Request, res: Response) => {
+const getAll = async (_req: Request, res: Response) => {
+  const products = await getAllProducts();
+  res.status(200).json({ data: products });
+};
+
+const getById = async (req: Request, res: Response) => {
   const { id } = req.params;
   const product = await getProductById(+id);
   if (!product) return res.status(404).json({ message: "Product not found" });
   return res.status(200).json({ product });
-};
-
-const getById = async (req: Request, resp: Response) => {
-  const { id } = req.params;
-  const product = await getProductById(+id);
-  if (!product) return resp.status(404).json({ message: "Product not found" });
-  return resp.status(200).json({ product });
 };
 
 export { getAll, getById };
