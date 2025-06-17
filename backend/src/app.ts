ff9bdfe1b1f619e9b540c49bb286de67ce3fd832
João Lee <joaumlimaum@gmail.com>
2025-06-17 12:45:37 -0300

    feat: configure cors

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,7 +1,10 @@
 import express, { type Application } from "express";
 import productRoutes from "./routes/product.route";
+import cors from 'cors'
 
 const app: Application = express();
+
+app.use(cors({ origin: "http://localhost:5173" })); 
 app.use(express.json());
 
 app.use("/", productRoutes);
