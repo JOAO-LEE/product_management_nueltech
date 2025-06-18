a868efe8e7b9100994cb67fbdb62bb732d367539
João Lee <joaumlimaum@gmail.com>
2025-06-18 10:14:33 -0300

    style: button styles

--- frontend/src/ui/Button/Button.tsx
+++ frontend/src/ui/Button/Button.tsx
@@ -1,9 +1,13 @@
 import type { ComponentProps, ReactNode } from "react";
+import "./Button.css"
 
 export default function Button({
   children,
   ...rest
 }: { children: ReactNode } & ComponentProps<"button">) {
-  return <button {...rest}>{children}</button>;
+  return (
+    <button className="button" {...rest}>
+      {children}
+    </button>
+  );
 }
- 
