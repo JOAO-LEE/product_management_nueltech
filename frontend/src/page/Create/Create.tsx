c8c0f4153aba165a2d93324b6dedef2596e4f399
João Lee <joaumlimaum@gmail.com>
2025-06-18 01:17:20 -0300

    feat: add create product page

---
+++ frontend/src/page/Create/Create.tsx
@@ -0,0 +1,94 @@
+import type { CreateProduct } from "../../types/Product";
+import { InputField, InputRoot } from "../../ui/Input/Input";
+import Button from "../../ui/Button/Button";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { createProductSchema } from "../../schema/product.schema";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { createProduct } from "../../service/productService";
+import { useNavigate } from "react-router";
+
+export function Create() {
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+
+    formState: { errors },
+  } = useForm<CreateProduct>({
+    resolver: zodResolver(createProductSchema),
+  });
+
+  const onSubmit: SubmitHandler<CreateProduct> = async (data) => {
+    try {
+      const parsedValue = createProductSchema.safeParse(data);
+      if (!parsedValue.success) {
+        throw new Error(parsedValue.error.message);
+      }
+      await createProduct(parsedValue.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <section>
+      <i className="ph ph-arrow-square-left" onClick={() => navigate("/")}></i>
+      <div>
+        <p>Crie um produto!</p>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <InputRoot>
+            <InputField
+              {...register("name")}
+              name="name"
+              type="text"
+              placeholder="Nome"
+            />
+          </InputRoot>
+          {errors.name && <p>{errors.name.message}</p>}
+
+          <InputRoot>
+            <InputField
+              {...register("description")}
+              name="description"
+              type="text"
+              placeholder="Descrição"
+            />
+          </InputRoot>
+          {errors.category && <p>{errors.category.message}</p>}
+
+          <InputRoot>
+            <InputField
+              {...register("category")}
+              name="category"
+              type="text"
+              placeholder="Categoria"
+            />
+          </InputRoot>
+          {errors.category && <p>{errors.category.message}</p>}
+
+          <InputRoot>
+            <InputField
+              {...register("stock", { valueAsNumber: true })}
+              name="stock"
+              type="text"
+              placeholder="Estoque"
+            />
+          </InputRoot>
+          {errors.stock && <p>{errors.stock.message}</p>}
+
+          <InputRoot>
+            <InputField
+              {...register("price", { valueAsNumber: true })}
+              name="price"
+              type="text"
+              placeholder="Preço"
+            />
+          </InputRoot>
+          {errors.price && <p>{errors.price.message}</p>}
+          <Button type="submit">Criar</Button>
+        </form>
+      </div>
+    </section>
+  );
+}
