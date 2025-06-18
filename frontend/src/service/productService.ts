a8cae9f9ece73837747a69eccd9196ca3eb9be0e
João Lee <joaumlimaum@gmail.com>
2025-06-18 10:22:29 -0300

    feat: add update and create service functions
    fix/chore: get all products base url - remove unused imports

--- frontend/src/service/productService.ts
+++ frontend/src/service/productService.ts
@@ -1,4 +1,4 @@
-import type { CreateProduct, Product } from "../types/Product";
+import type { CreateProduct, Product, UpdateProduct } from "../types/Product";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function fetchAllProducts(): Promise<{
@@ -31,7 +31,7 @@ export async function createProduct(product: CreateProduct): Promise<{
   success: boolean;
   data: Product;
 }> {
-  const response = await fetch("http://localhost:3000", {
+  const response = await fetch(`${API_BASE_URL}`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -45,4 +45,45 @@ export async function createProduct(product: CreateProduct): Promise<{
 
   const result = await response.json();
   return result;
+}
+
+export async function updateProduct(product: UpdateProduct): Promise<{
+  success: boolean;
+  data: Product;
+}> {
+  const response = await fetch(`${API_BASE_URL}/${product.id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create a product");
+  }
+
+  const result = await response.json();
+  return result;
+}
+
+export async function deleteProduct(
+  id: number
+): Promise<{ message: string; success: true }> {
+  const response = await fetch(`${API_BASE_URL}/${id}`, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error(response.status.toString());
+  }
+
+  console.log(response);
+  return {
+    message: "Produto deletado com sucesso!",
+    success: response.ok,
+  };
 }
