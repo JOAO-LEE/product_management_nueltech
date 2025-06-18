0f762122511402b3f8e7e65447f5a25526a4cb74
João Lee <joaumlimaum@gmail.com>
2025-06-18 01:16:00 -0300

    feat: add create product service function

--- frontend/src/service/productService.ts
+++ frontend/src/service/productService.ts
@@ -1,4 +1,4 @@
-import type { Product } from "../interface/product";
+import type { CreateProduct, Product } from "../types/Product";
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export async function fetchAllProducts(): Promise<{
@@ -21,6 +21,26 @@ export async function fetchProductById(productId: number): Promise<{
   const response = await fetch(`${API_BASE_URL}/${productId}`);
   if (!response.ok) {
     throw new Error("Failed to fetch products");
+  }
+
+  const result = await response.json();
+  return result;
+}
+
+export async function createProduct(product: CreateProduct): Promise<{
+  success: boolean;
+  data: Product;
+}> {
+  const response = await fetch("http://localhost:3000", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(product),
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to create a product");
   }
 
   const result = await response.json();
