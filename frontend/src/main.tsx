4bf5436a10b1b297ba09ac8109cd7d4fbe7edecd
João Lee <joaumlimaum@gmail.com>
2025-06-18 04:02:11 -0300

    feat: configure toast provider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./reset.css";
 import { BrowserRouter } from "react-router";
+import { ToastProvider } from "./context/ToastProvider.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ToastProvider>
+        <App />
+      </ToastProvider>
     </BrowserRouter>
   </StrictMode>
 );
