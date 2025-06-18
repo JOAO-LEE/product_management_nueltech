4157bd29b7b85c3c3ead44a6a0db6e3e33126963
João Lee <joaumlimaum@gmail.com>
2025-06-18 10:15:37 -0300

    style: input styles

--- frontend/src/ui/Input/Input.tsx
+++ frontend/src/ui/Input/Input.tsx
@@ -1,12 +1,14 @@
 import { type ComponentProps } from "react";
+import "./Input.css";
+import type { FieldError } from "react-hook-form";
 
 export function InputField({ ...rest }: ComponentProps<"input">) {
-  return <input {...rest} />;
+  return <input {...rest} className="input-field" />;
 }
 
 export function InputRoot({
   error = false,
   ...rest
-}: { error?: boolean } & ComponentProps<"div">) {
-  return <div {...rest} data-error={error} />;
+}: { error?: FieldError } & ComponentProps<"div">) {
+  return <div {...rest} data-error={error} className="input-wrapper" />;
 }
