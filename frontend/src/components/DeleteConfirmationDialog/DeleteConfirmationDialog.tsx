f5958bb97b058b4c7c1406dd07abceb487ed625c
João Lee <joaumlimaum@gmail.com>
2025-06-18 10:11:25 -0300

    chore: add styles and remove unused imports

--- frontend/src/components/DeleteConfirmationDialog/DeleteConfirmationDialog.tsx
+++ frontend/src/components/DeleteConfirmationDialog/DeleteConfirmationDialog.tsx
@@ -1,6 +1,9 @@
-import type { RefObject } from "react";
-import type { Product } from "../../interface/product";
-
+import { useContext, type RefObject } from "react";
+import type { Product } from "../../types/Product";
+import { deleteProduct } from "../../service/productService";
+import "./DeleteConfirmationDialog.css";
+import { ToastContext } from "../../context/ToastContext";
+import { useNavigate } from "react-router";
 export function DeleteConfirmationDialog({
   dialogRef,
   product,
@@ -8,24 +11,38 @@ export function DeleteConfirmationDialog({
   dialogRef: RefObject<HTMLDialogElement | null>;
   product: Product | null;
 }) {
+  const { createToast } = useContext(ToastContext);
+  const navigate = useNavigate();
+
+  const confirmDelete = async () => {
+    const confirmation = await deleteProduct(product!.id);
+    if (confirmation) {
+      dialogRef.current?.close();
+      createToast({
+        type: confirmation.success && "success",
+        message: confirmation.message,
+      });
+      navigate("/");
+    }
+  };
   return (
-    <dialog
-      ref={dialogRef}
-      style={{
-        color: "white",
-        backgroundColor: "black",
-        width: "50%",
-        height: "25%",
-        backdropFilter: "blur(1rem)",
-        alignContent: "center",
-        textAlign: "center",
-      }}
-    >
-      Você confirma que deseja confirmar o produto abaixo?
-      <p>{product?.name}</p>
-      <p>{product?.price}</p>
-      <button>Sim</button>
-      <button onClick={() => dialogRef.current?.close()}>Não</button>
+    <dialog ref={dialogRef} className="delete-dialog">
+      Você confirma que deseja deletar o produto abaixo?
+      <div className="product-info">
+        <p>{product?.name}</p>
+        <p>R$ {product?.price}</p>
+      </div>
+      <div className="dialog-actions">
+        <button className="dialog-buttons" onClick={() => confirmDelete()}>
+          Sim
+        </button>
+        <button
+          className="dialog-buttons"
+          onClick={() => dialogRef.current?.close()}
+        >
+          Não
+        </button>
+      </div>
     </dialog>
   );
 }
