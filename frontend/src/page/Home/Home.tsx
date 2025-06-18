33a4240ea17268f707f565a4d741ddef1da90d85
João Lee <joaumlimaum@gmail.com>
2025-06-18 07:59:03 -0300

    style: home page style

--- frontend/src/page/Home/Home.tsx
+++ frontend/src/page/Home/Home.tsx
@@ -1,10 +1,15 @@
 import { useState, useEffect } from "react";
-import { ProductTable } from "../../components/ProductTable";
-import type { Product } from "../../interface/product";
+import { ProductTable } from "../../components/ProductsTable/ProductsTable";
+import type { Product } from "../../types/Product";
 import { fetchAllProducts } from "../../service/productService";
+import Button from "../../ui/Button/Button";
+import { useNavigate } from "react-router";
+import "./Home.css";
 
 export function Home() {
   const [products, setProducts] = useState<Product[]>([]);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     fetchAllProducts()
@@ -13,7 +18,13 @@ export function Home() {
   }, []);
 
   return (
-    <section>
+    <section className="home-container">
+      <div>
+        <h1>Lista de produtos</h1>
+        <Button onClick={() => navigate("/create")}>
+          Criar novo produto <i className="ph ph-plus"></i>
+        </Button>
+      </div>
       <ProductTable products={products} />
     </section>
   );
